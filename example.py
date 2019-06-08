fe74dd445200d6499136cd8f4e70ba33350c625d
glebarez <gleb.sakhnov@gmail.com>
2019-06-08 13:39:32 +0300

    example

---
+++ example.py
@@ -0,0 +1,32 @@
+from time import sleep
+from perfy import perfy
+
+
+@perfy # <-- use decorator to track function calls
+def func_sleep():
+    sleep(.02)
+    
+
+def sleep_loop():
+    for _ in range(10):
+        with perfy('sleep loop body'): # <-- use with-statement to track arbitrary block of code
+            func_sleep()
+
+# you can nest with-blocks and decorated function calls in any order:
+@perfy # <-- a decorator on a top level function
+def main():
+    sleep_loop() # <-- this functions has a traced block inside
+
+    with perfy('custom named block'): # <-- traced block
+        sleep(.1)
+        
+        with perfy('inner block'): # <-- nested traced block
+            func_sleep()
+            func_sleep()
+                
+if __name__ == '__main__':
+    # run top-level function
+    main()
+
+    # let perfy print report
+    perfy.report()
