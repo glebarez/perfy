967c78be8705ee2c764c2deb4c2b5d00ba3d6265
Сахнов Глеб Андреевич <14505283@DSL-WSD-0002824.sigma.sbrf.ru>
2019-09-06 12:22:37 +0300

    + average seconds

--- perfy/__init__.py
+++ perfy/__init__.py
@@ -12,7 +12,11 @@ from time import perf_counter
 from functools import wraps
 from copy import deepcopy
 
-__version__ = '0.27'
+__version__ = '0.28'
+
+# dict-key aliases
+_TIME = '#'
+_COUNT = '~'
 
 class Perfy():
     '''Perfy - a simple performance tracer
@@ -30,7 +34,7 @@ class Perfy():
     def _context_manager(self, descr):
         # initialize current stack segment, ifn
         if descr not in self._sp:
-            self._sp[descr] = {'#':0, '~':0}
+            self._sp[descr] = {_TIME:0, _COUNT:0}
         
         # stash current stack pointer and get deeper
         _upper_sp  = self._sp
@@ -43,8 +47,8 @@ class Perfy():
         s = perf_counter() - t0
 
         # update numbers
-        self._sp['#'] += s
-        self._sp['~'] += 1
+        self._sp[_TIME] += s
+        self._sp[_COUNT] += 1
         
         # restore stack pointer
         self._sp = _upper_sp
@@ -70,8 +74,8 @@ class Perfy():
     
     # prints a report
     def report(self):
-        print('{0:-^80}'.format('Perfy report'))
-        print('{:^40}{:^20}{:^20}'.format('Function/Method', 'Time(sec.)', 'Calls(count)'))
+        print('\n{0:-^80}\n'.format(' Perfy report '))
+        print('{:^35}{:^15}{:^15}{:^15}'.format('Function/Method', 'Time(sec.)', 'Calls(count)', 'Avg(sec.)'))
         print('{:-^80}'.format(''))
         if self._stack:
             self._report(deepcopy(self._stack), level = 0)
@@ -82,8 +86,11 @@ class Perfy():
     # inner recursive implementation of report prinitng
     def _report(self, stack, level):
         for name, stats in sorted(stack.items(), key = lambda x:x[1]['#'], reverse = True):
-            print('{:40}{:^20.3f}{:^20,d}'.format(
-                '   ' * level + ('└ ' if level else '') + name, stats.pop('#'), stats.pop('~')))
+            _time = stats.pop(_TIME)
+            _count = stats.pop(_COUNT)
+            _avg = round(_time/_count,3) if _count else 0
+            print('{:35}{:>15.3f}{:>15,d}{:>15.3f}'.format(
+                '   ' * level + ('└ ' if level else '') + name, _time, _count,_avg))
             self._report(stats, level + 1)
 
 # a module-level instance for convenience
